54ca2ff65299c28678998f11c7639e203334c2e9
mpobletemori <mpobletemori@gmail.com>
2022-04-29 06:32:48 -0500

    uso de metodo subscribe()

--- 1-spring-boot-reactor/src/main/java/ccom/example/springboot/reactor/app/SpringBootReactorApplication.java
+++ 1-spring-boot-reactor/src/main/java/ccom/example/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,5 +1,6 @@
 package ccom.example.springboot.reactor.app;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,14 +8,19 @@ import reactor.core.publisher.Flux;
 
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
-
+	private static final Logger log = LoggerFactory.getLogger(SpringBootReactorApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
-		Flux<String> nombres = Flux.just("Manuel","Santi","Betty","Eli","Antonio").doOnNext(System.out::println);
-		nombres.subscribe();
+		Flux<String> nombres = Flux.just("Manuel","Santi","Betty","","Eli","Antonio").doOnNext(value->{
+			if(value.isEmpty()){
+				throw new RuntimeException("Nombres no puede ser validado");
+			}
+            System.out.println(value);
+		});
+		nombres.subscribe(log::info,error->log.error(error.getMessage()));
 	}
 }
