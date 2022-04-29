ad67ea762d0a292798c84681d91990241f43133d
mpobletemori <mpobletemori@gmail.com>
2022-04-29 06:21:27 -0500

    Flux observable

--- 1-spring-boot-reactor/src/main/java/ccom/example/springboot/reactor/app/SpringBootReactorApplication.java
+++ 1-spring-boot-reactor/src/main/java/ccom/example/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,13 +1,20 @@
 package ccom.example.springboot.reactor.app;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Flux;
 
 @SpringBootApplication
-public class SpringBootReactorApplication {
+public class SpringBootReactorApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootReactorApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Flux<String> nombres = Flux.just("Manuel","Santi","Betty","Eli","Antonio").doOnNext(System.out::println);
+		nombres.subscribe();
+	}
 }
