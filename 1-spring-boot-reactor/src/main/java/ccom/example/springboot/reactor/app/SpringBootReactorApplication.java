9ca23449f1e61d92bcdb436f92bb8a6151ca62a9
mpobletemori <mpobletemori@gmail.com>
2022-05-10 11:53:25 -0500

    uso de map en reactor

--- 1-spring-boot-reactor/src/main/java/ccom/example/springboot/reactor/app/SpringBootReactorApplication.java
+++ 1-spring-boot-reactor/src/main/java/ccom/example/springboot/reactor/app/SpringBootReactorApplication.java
@@ -1,10 +1,13 @@
 package ccom.example.springboot.reactor.app;
+import ccom.example.springboot.reactor.app.models.Usuario;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
+
+import java.util.Objects;
 
 @SpringBootApplication
 public class SpringBootReactorApplication implements CommandLineRunner {
@@ -15,13 +18,26 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		Flux<String> nombres = Flux.just("Manuel","Santi","Betty","Shobi","Eli","Antonio").doOnNext(value->{
-			if(value.isEmpty()){
-				throw new RuntimeException("Nombres no puede ser validado");
-			}
-            System.out.println(value);
-		});
-		nombres.subscribe(log::info, error -> log.error(error.getMessage()), new Runnable() {
+		Flux<Usuario> nombres = Flux.just("Manuel","Santi","Betty","Shobi","Eli","Antonio")
+				.map(nombre->{
+					var nombreUpperCase =nombre.toUpperCase();
+					log.info("map="+nombreUpperCase);
+					return new Usuario(nombreUpperCase,null);
+				})
+				.doOnNext(usuario->{
+					if(Objects.isNull(usuario)){
+						log.error("campo vacio");
+						throw new RuntimeException("Nombres no puede ser validado");
+					}
+					log.info("doOnNext="+usuario.getNombre());
+				}).map(usuario->{
+					var nombreLowerCase =usuario.getNombre().toLowerCase();
+					log.info("map="+nombreLowerCase);
+					usuario.setNombre(nombreLowerCase);
+					return usuario;
+				});
+
+		nombres.subscribe((usuario)->log.info(usuario.toString()), error -> log.error(error.getMessage()), new Runnable() {
 			@Override
 			public void run() {
 				//se lanza cuando se procesa el total de lista
