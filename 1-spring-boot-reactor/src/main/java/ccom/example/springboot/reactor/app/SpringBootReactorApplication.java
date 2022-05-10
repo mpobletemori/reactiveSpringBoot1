dfc782b7adb79192a594fcee94e48d00168f6c6d
mpobletemori <mpobletemori@gmail.com>
2022-05-10 14:10:48 -0500

    uso de flatmap

--- 1-spring-boot-reactor/src/main/java/ccom/example/springboot/reactor/app/SpringBootReactorApplication.java
+++ 1-spring-boot-reactor/src/main/java/ccom/example/springboot/reactor/app/SpringBootReactorApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.List;
 import java.util.Objects;
@@ -19,8 +20,39 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		//ejemploIterable();
+		ejemploFlatMap();
+	}
+
+	public void ejemploFlatMap() throws Exception {
+		log.info("Ejemplo flatMap");
 		List<String> listNombres = List.of("Manuel Poblete","Santi Poblete","Betty Cuenca","Shobi Ribeiro","Eli Mori","Antonio Sanchez","Bruce Lee","Bruce Willis");
-		//Flux<String> nombres = Flux.just("Manuel Poblete","Santi Poblete","Betty Cuenca","Shobi Ribeiro","Eli Mori","Antonio Sanchez","Bruce Lee","Bruce Willis");
+
+		Flux.fromIterable(listNombres)
+				.map(nombre->{
+					var nombreUpperCase =nombre.toUpperCase();
+					log.info("map="+nombreUpperCase);
+					return new Usuario(nombreUpperCase.split(" ")[0],nombreUpperCase.split(" ")[1]);
+				})
+				.flatMap(usuario-> {
+					if(usuario.getNombre().equalsIgnoreCase("bruce")){
+						log.info("flatMap="+usuario.toString()+",mantener elemento en observable");
+						return Mono.just(usuario);
+					}
+					log.info("flatMap="+usuario.toString()+", sacar elemento en observable");
+					return Mono.empty();
+				})
+				.map(usuario->{
+					var nombreLowerCase =usuario.getNombre().toLowerCase();
+					log.info("map="+nombreLowerCase);
+					usuario.setNombre(nombreLowerCase);
+					return usuario;
+				}).subscribe(usuario->log.info(usuario.toString()));
+	}
+
+
+	public void ejemploIterable() throws Exception {
+		List<String> listNombres = List.of("Manuel Poblete","Santi Poblete","Betty Cuenca","Shobi Ribeiro","Eli Mori","Antonio Sanchez","Bruce Lee","Bruce Willis");
 		Flux<String> nombres = Flux.fromIterable(listNombres);
 		Flux<Usuario> usuarios =nombres.map(nombre->{
 					var nombreUpperCase =nombre.toUpperCase();
@@ -28,9 +60,9 @@ public class SpringBootReactorApplication implements CommandLineRunner {
 					return new Usuario(nombreUpperCase.split(" ")[0],nombreUpperCase.split(" ")[1]);
 				})
 				.filter(usuario-> {
-					 var value = usuario.getNombre().equalsIgnoreCase("bruce");
-					 log.info("filter="+usuario.toString()+",result="+value);
-					 return value;
+					var value = usuario.getNombre().equalsIgnoreCase("bruce");
+					log.info("filter="+usuario.toString()+",result="+value);
+					return value;
 				})
 				.doOnNext(usuario->{
 					if(Objects.isNull(usuario)){
