dcd3902a76520cf9c428d97c0d64953c7c424b8b
mpobletemori <mpobletemori@gmail.com>
2022-05-17 11:10:42 -0500

    suscribir otro observador y modificando stream reactivo

--- 2-spring-boot-webflux/src/main/java/com/example/springbootwebflux/controller/ProductoController.java
+++ 2-spring-boot-webflux/src/main/java/com/example/springbootwebflux/controller/ProductoController.java
@@ -3,6 +3,8 @@ package com.example.springbootwebflux.controller;
 import com.example.springbootwebflux.models.documents.ProductoDocument;
 import com.example.springbootwebflux.models.repository.ProductoRepository;
 import org.hibernate.validator.constraints.CodePointLength;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,12 +13,19 @@ import reactor.core.publisher.Flux;
 
 @Controller
 public class ProductoController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
     @Autowired
     private ProductoRepository productoRepository;
 
     @GetMapping({"/listar","/"})
     public String listar(Model model){
-        Flux<ProductoDocument> productos = productoRepository.findAll();
+        Flux<ProductoDocument> productos = productoRepository.findAll().map(producto->{
+            producto.setNombre(producto.getNombre().toUpperCase());
+            return producto;
+        });
+
+        productos.subscribe(producto->LOGGER.info(producto.getNombre()));
         model.addAttribute("productos", productos);
         model.addAttribute("titulo", "Listado de productos");
         return "listar";
