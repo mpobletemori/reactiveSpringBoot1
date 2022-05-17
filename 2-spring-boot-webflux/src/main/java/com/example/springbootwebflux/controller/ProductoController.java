250e098fb32d6b143b9de1fd7309610cc175d80c
mpobletemori <mpobletemori@gmail.com>
2022-05-17 11:18:36 -0500

    Modo reactive data driver para manejar la contrapresion

--- 2-spring-boot-webflux/src/main/java/com/example/springbootwebflux/controller/ProductoController.java
+++ 2-spring-boot-webflux/src/main/java/com/example/springbootwebflux/controller/ProductoController.java
@@ -9,7 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 import reactor.core.publisher.Flux;
+
+import java.time.Duration;
 
 @Controller
 public class ProductoController {
@@ -27,6 +30,19 @@ public class ProductoController {
 
         productos.subscribe(producto->LOGGER.info(producto.getNombre()));
         model.addAttribute("productos", productos);
+        model.addAttribute("titulo", "Listado de productos");
+        return "listar";
+    }
+
+    @GetMapping("/listar-datadriver")
+    public String listarDataDriver(Model model){
+        Flux<ProductoDocument> productos = productoRepository.findAll().map(producto->{
+            producto.setNombre(producto.getNombre().toUpperCase());
+            return producto;
+        }).delayElements(Duration.ofSeconds(1));
+
+        productos.subscribe(producto->LOGGER.info(producto.getNombre()));
+        model.addAttribute("productos", new ReactiveDataDriverContextVariable(productos,1));
         model.addAttribute("titulo", "Listado de productos");
         return "listar";
     }
