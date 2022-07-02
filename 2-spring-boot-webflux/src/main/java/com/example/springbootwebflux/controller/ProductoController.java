2eda9ca65159d0810fa7863048b9100a929c9a05
mpobletemori <mpobletemori@gmail.com>
2022-07-02 12:51:38 -0500

    implementar logica editar producto

--- 2-spring-boot-webflux/src/main/java/com/example/springbootwebflux/controller/ProductoController.java
+++ 2-spring-boot-webflux/src/main/java/com/example/springbootwebflux/controller/ProductoController.java
@@ -7,14 +7,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 import org.thymeleaf.spring5.context.webflux.ReactiveDataDriverContextVariable;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.time.Duration;
 
+@SessionAttributes("producto")
 @Controller
 public class ProductoController {
 
@@ -36,11 +37,42 @@ public class ProductoController {
     public Mono<String> crear(Model model){
         model.addAttribute("producto",new ProductoDocument());
         model.addAttribute("titulo", "Listado de productos");
+        model.addAttribute("boton", "Crear");
         return Mono.just("form");
     }
 
+    @GetMapping("/form/{id}")
+    public Mono<String> editar(@PathVariable String id, Model model){
+        Mono<ProductoDocument>  productoMono = productoService.findById(id).doOnNext(p->{
+            LOGGER.info("producto encontrado: {}",p.getNombre());
+        }).defaultIfEmpty(new ProductoDocument());
+        model.addAttribute("titulo", "Editar producto");
+        model.addAttribute("boton", "Editar");
+        model.addAttribute("producto",productoMono);
+
+        return Mono.just("form");
+    }
+
+    @GetMapping("/form-v2/{id}")
+    public Mono<String> editarV2(@PathVariable String id, Model model){
+        return productoService.findById(id).doOnNext(p->{
+            LOGGER.info("producto encontrado: {}",p.getNombre());
+            model.addAttribute("titulo", "Editar producto");
+            model.addAttribute("boton", "Editar");
+            model.addAttribute("producto",p);
+        }).defaultIfEmpty(new ProductoDocument())
+                .flatMap(p->{
+                    if(p.getId() == null){
+                        return Mono.error(new InterruptedException("No existe producto"));
+                    }
+                    return Mono.just(p);
+                }).then(Mono.just("form"))
+                .onErrorResume(ex->Mono.just("redirect:/listar?error=No+existe+el+producto"));
+    }
+
     @PostMapping("/form")
-    public Mono<String> guardar(ProductoDocument producto){
+    public Mono<String> guardar(ProductoDocument producto , SessionStatus sessionStatus){
+            sessionStatus.setComplete();
             return this.productoService.save(producto).doOnNext(p->{
                 LOGGER.info("Producto guardado {} , id: {}",p.getNombre(),p.getId());
             }).thenReturn("redirect:/listar");
