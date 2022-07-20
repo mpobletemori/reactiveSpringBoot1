0636286289b2425adc44f95063b17c6e69aa0a35
mpobletemori <mpobletemori@gmail.com>
2022-07-20 06:02:57 -0500

    implementar ver detalle y crear full reactivo

--- 3.-spring-boot-webflux-apirest/src/main/java/com/example/springbootwebfluxapirest/app/RouterFunctionConfig.java
+++ 3.-spring-boot-webflux-apirest/src/main/java/com/example/springbootwebfluxapirest/app/RouterFunctionConfig.java
@@ -4,6 +4,7 @@ import com.example.springbootwebfluxapirest.app.handler.ProductoHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.reactive.function.server.RouterFunction;
 
 import org.springframework.web.reactive.function.server.ServerResponse;
@@ -16,8 +17,9 @@ public class RouterFunctionConfig {
 
     @Bean
     public RouterFunction<ServerResponse> routes(ProductoHandler productoHandler){
-        return route(GET("/api/v2/productos").or(GET("/api/v3/productos")),
-                request->productoHandler.listar(request));
+        return route(GET("/api/v2/productos").or(GET("/api/v3/productos")),request->productoHandler.listar(request))
+                .andRoute(GET("/api/v2/productos/{id}").and(contentType(MediaType.APPLICATION_JSON)), request->productoHandler.ver(request))
+                .andRoute(POST("/api/v2/productos"),request->productoHandler.crear(request));
     }
 
 }
--- 3.-spring-boot-webflux-apirest/src/main/java/com/example/springbootwebfluxapirest/app/handler/ProductoHandler.java
+++ 3.-spring-boot-webflux-apirest/src/main/java/com/example/springbootwebfluxapirest/app/handler/ProductoHandler.java
@@ -5,9 +5,13 @@ import com.example.springbootwebfluxapirest.app.models.service.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import reactor.core.publisher.Mono;
+
+import java.net.URI;
+import java.util.Date;
 
 @Component
 public class ProductoHandler {
@@ -21,6 +25,32 @@ public class ProductoHandler {
                 .contentType(MediaType.APPLICATION_JSON_UTF8)
                 .body(this.productoService.findAll(), ProductoDocument.class);
     }
+
+    public Mono<ServerResponse> ver(ServerRequest request){
+        String id = request.pathVariable("id");
+        return productoService.findById(id).flatMap(p->
+                ServerResponse
+                        .ok()
+                        .contentType(MediaType.APPLICATION_JSON_UTF8)
+                        .body(BodyInserters.fromObject(p)))
+                .switchIfEmpty(ServerResponse.notFound().build());
+    }
+
+    public Mono<ServerResponse> crear(ServerRequest request){
+        Mono<ProductoDocument> productoMono = request.bodyToMono(ProductoDocument.class);
+
+        return productoMono.flatMap(p->{
+           if(p.getCreateAt() == null){
+               p.setCreateAt(new Date());
+           }
+           return this.productoService.save(p);
+        }).flatMap(p-> ServerResponse.created(URI.create("/api/v2/productos/".concat(p.getId())))
+                                     .contentType(MediaType.APPLICATION_JSON_UTF8)
+                                     .body(BodyInserters.fromObject(p))
+        );
+    }
+
+
 
 
 }
